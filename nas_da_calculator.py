31b201c947382cf043b2f9370bd7638891a066e4
adamfox <adamrfox@yahoo.com>
2021-06-14 17:02:36 -0400

    Fixed division to float issue with Python 2.x

--- nas_da_calculator.py
+++ nas_da_calculator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from __future__ import print_function
+from __future__ import division
 import rubrik_cdm
 import sys
 import getopt
@@ -64,10 +65,16 @@ if __name__ == '__main__':
         rubrik = rubrik_cdm.Connect(rubrik_node, api_token=token)
     else:
         rubrik = rubrik_cdm.Connect(rubrik_node, user, password)
-    rubrik_config = rubrik.get('v1', '/cluster/me', timeout=60)
-    rubrik_tz = rubrik_config['timezone']['timezone']
-    local_zone = pytz.timezone(rubrik_tz)
-    utz_zone = pytz.timezone('utc')
+    if not latest:
+        rubrik_config = rubrik.get('v1', '/cluster/me', timeout=60)
+        print ("TZ= " + rubrik_config['timezone']['timezone'])
+        rubrik_tz = pytz.timezone(rubrik_config['timezone']['timezone'])
+        local_zone = pytz.timezone(str(rubrik_tz))
+        utz_zone = pytz.timezone('utc')
+        date_tz = rubrik_tz.localize(date)
+        date_utc = date_tz.astimezone(pytz.utc).replace(tzinfo=None)
+        date_utc = (date_utc - datetime.datetime(1970,1,1)).total_seconds()
+        print ("DATE: " + str(date_utc))
     done = False
     offset = 0
     while not done:
@@ -90,18 +97,33 @@ if __name__ == '__main__':
             offset = fs_data['offset']
     total_size = 0
     for fs in filesets:
+        print("FS: " + str(fs['share']) + " // " + fs['id'])
         fs_info = rubrik.get('v1', '/fileset/' + str(fs['id']))
         if latest:
-            snap_id = fs_info['snapshots'][-1]['id']
-            print ("ID= " + snap_id)
-            exit(2)
+            try:
+                snap_id = fs_info['snapshots'][-1]['id']
+            except IndexError:
+                print("No Backups")
+                continue
         else:
+            snap_date = ""
+            snap_id = ""
             for snap in fs_info['snapshots']:
-                print("ID: " + str(snap['id']) + " // " + snap['date'])
-            exit(1)
-        snap_info= rubrik.get('v1', '/fileset/snapshot/' + str(snap_id))
+                snap_date = datetime.datetime.strptime(snap['date'][:-5], "%Y-%m-%dT%H:%M:%S")
+                print ("\t" + str(snap_date))
+                snap_date_ep = (snap_date - datetime.datetime(1970,1,1)).total_seconds()
+                print ("\t" + str(snap_date_ep))
+                if snap_date_ep >= date_utc:
+                    print("SNAP_DATE: = " + str(snap_date))
+                    snap_id = snap['id']
+                    break
+        try:
+            snap_info = rubrik.get('v1', '/fileset/snapshot/' + str(snap_id))
+        except:
+            continue
         fs['size'] = snap_info['size']
         total_size += fs['size']
         print(fs['host'] + ":" + fs['share'] + "," + bytes_to_tb(fs['size']))
     print ("Total:" + "," + bytes_to_tb(total_size))
 
+##TODO Bad logic on date.  Must scan and find the first one that lives at or before the date given
